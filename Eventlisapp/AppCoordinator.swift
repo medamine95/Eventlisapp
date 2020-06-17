3a25ca34837f253785ceb24364b98860ceedff70
medamine95 <m.a.benbelgacem@mintit.io>
2020-06-17 12:30:15 +0100

    init

---
+++ Eventlisapp/AppCoordinator.swift
@@ -0,0 +1,31 @@
+//
+//  AppCoordinator.swift
+//  Eventlisapp
+//
+//  Created by MacBook Pro on 6/17/20.
+//  Copyright © 2020 Mint. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators:[Coordinator] {get}
+    func start()
+}
+
+final class Appcoordinator:Coordinator{
+   private(set) var childCoordinators: [Coordinator] = []
+    
+    private let window:UIWindow
+    
+    
+    func start() {
+        let  navigationController = UINavigationController()
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+    
+    
+    
+}
---
+++ Eventlisapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eventlisapp
+//
+//  Created by MacBook Pro on 6/17/20.
+//  Copyright © 2020 Mint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
